8666beec9b27c7993cfafe267fe3d54766a16e95
ZeeyahOke <f.oke@alustudent.com>
2026-07-15 22:53:17 +0200

    feat: Game manager added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,71 @@
 using UnityEngine;
 
+public interface IDamageable
+{
+    void TakeDamage(int amount);
+}
+
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager Instance { get; private set; }
+
+    [Header("Round Timer")]
+    [SerializeField] private float roundDuration = 60f;
+
+    private int score;
+    private float timeRemaining;
+    private bool isRoundActive;
+
+    public int Score => score;
+    public float TimeRemaining => timeRemaining;
+    public bool IsRoundActive => isRoundActive;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        Time.timeScale = 1f;
+        timeRemaining = roundDuration;
+        isRoundActive = true;
+        score = 0;
+    }
+
     void Update()
     {
-        
+        if (!isRoundActive) return;
+
+        timeRemaining -= Time.deltaTime;
+
+        if (timeRemaining <= 0f)
+        {
+            timeRemaining = 0f;
+            EndRound();
+        }
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+    }
+
+    public void OnPlayerDied()
+    {
+        if (!isRoundActive) return;
+        EndRound();
+    }
+
+    private void EndRound()
+    {
+        isRoundActive = false;
+        Time.timeScale = 0f;
     }
 }
